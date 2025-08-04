9fb274a81e355cd49d8c5f14d6f35190713ec84d
Anthony M <m_tuiono@yahoo.com>
2025-08-03 23:04:46 -0700

    Update players.py

--- players.py
+++ players.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.code(line_number=False)
+#st.code(line_number=False)
 
 st.title("Tonga USA Golf Tournament 2025 at Bluerock Golf Course, Vallejo CA")
 st.subheader("August 22nd and 23rd, 2025")
