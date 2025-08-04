76ee6b26c799407b889c279ad10e1a3012a6a568
Anthony M <m_tuiono@yahoo.com>
2025-08-03 23:09:44 -0700

    Update players.py

--- players.py
+++ players.py
@@ -7,6 +7,7 @@ st.title("Tonga USA Golf Tournament 2025 at Bluerock Golf Course, Vallejo CA")
 st.subheader("August 22nd and 23rd, 2025")
 st.subheader("Players List")
 st.write()
+st.divider()
 
 data = pd.read_csv("players.csv")
 st.write(data)
