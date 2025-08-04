b83f5cce7ead7cb036585dcda5cf88f07b40f892
Anthony M <m_tuiono@yahoo.com>
2025-08-03 22:36:50 -0700

    Update players.py

--- players.py
+++ players.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.write("Tonga USA Golf Tournament 2025 at Bluerock Golf Course, Vallejo CA")
+st.write("August 22nd and 23rd, 2025")
 st.write("Players List")
 st.write()
 
