437ad3bb651b92b2e4e3a53900127e34c3555b43
Anthony M <m_tuiono@yahoo.com>
2025-08-07 10:12:18 -0700

    Update players.py

--- players.py
+++ players.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 #st.code(line_number=False)
 
-st.image("logo2.jpg",width=80)
-st.title("TUGC 2025 Tournament")
+#st.image("logo2.jpg",width=80)
+st.title("TUGC 2025 Golf Tournament")
 st.subheader("Bluerock Golf Course,Vallejo CA, 8/22 and 8/23")
 st.write("Players List as of 8/7/25")
 st.write()
