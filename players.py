2246b2892722d23c94e8987dd2e0098b8731aa2c
Anthony M <m_tuiono@yahoo.com>
2025-08-07 09:01:48 -0700

    Update players.py

--- players.py
+++ players.py
@@ -1,14 +1,5 @@
 import streamlit as st
-from IPython.display import display, HTML
 import pandas as pd
-
-def create_scrollable_table(df, table_id, title, height='200px'):
-    html = f'<h3>{title}</h3>'
-    html += f'<div id="{table_id}" style="height:{height}; overflow:auto;">'
-    html += df.to_html()
-    html += '</div>'
-    return html
-
 
 #st.code(line_number=False)
 
@@ -20,18 +11,5 @@ st.write()
 st.divider()
 
 data = pd.read_csv("players.csv")
-
-df = pd.DataFrame(data) # Your DataFrame
-html_output = create_scrollable_table(df, 'my_dataframe_table', 'My Scrollable DataFrame', height='300px')
-display(HTML(html_output))
-
-#st.write(data)
-
-
-
-
-# Example usage:
-# 
-# 
-# 
+st.write(data)
 
