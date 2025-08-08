06eaf990981c3e31a76930353221835e0b506aaf
Anthony M <m_tuiono@yahoo.com>
2025-08-08 11:31:24 -0700

    Update players.py

--- players.py
+++ players.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
+import datetime
+
 
 #st.code(line_number=False)
 
 #st.image("logo2.jpg",width=80)
 st.title("TUGC 2025 Golf Tournament")
 st.subheader("Bluerock Golf Course,Vallejo CA, 8/22 and 8/23")
-st.write("Players List as of 8/8/25")
+st.write("Players List as of ",endl=" ")
+x = datetime.datetime.now()
+print(x)
 st.divider()
 
 data = pd.read_csv("players.csv")
