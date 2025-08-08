d6cc234fe1b300bb57bfc94d1f800e9ab77ad156
Anthony M <m_tuiono@yahoo.com>
2025-08-08 11:29:57 -0700

    Update players.py

--- players.py
+++ players.py
@@ -6,7 +6,7 @@ import pandas as pd
 #st.image("logo2.jpg",width=80)
 st.title("TUGC 2025 Golf Tournament")
 st.subheader("Bluerock Golf Course,Vallejo CA, 8/22 and 8/23")
-st.write("Players List as of 8/7/25")
+st.write("Players List as of 8/8/25")
 st.divider()
 
 data = pd.read_csv("players.csv")
