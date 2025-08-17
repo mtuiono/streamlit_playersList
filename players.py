24dd0e486cf5e4d62e96c591c1770325ed603a94
Anthony M <m_tuiono@yahoo.com>
2025-08-17 09:47:10 -0700

    Update players.py

--- players.py
+++ players.py
@@ -8,7 +8,7 @@ import datetime
 #st.image("logo2.jpg",width=80)
 st.title("TUGC 2025 Golf Tournament")
 st.subheader("Bluerock Golf Course,Vallejo CA, 8/22 and 8/23")
-st.write("Players List as of 8/16/25 ")
+st.write("Players List as of 8/17/25 ")
 
 
 st.divider()
